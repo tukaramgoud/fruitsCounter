1a04ed0561b5dbf0bcf82ab51c67342de7419bf2
tukaramgoud <tukaramgoud2804.com>
2024-03-26 06:03:35 +0000

    adding initial files

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+// Write your code here
+import './index.css'
+
+class FruitsCounter extends Component {
+  render() {
+    return (
+      <div className="background-container">
+        <div className="inner-background-container">
+          <h1>
+            Bob ate <span className="count-of-fruits"> 0 </span>Mangos and
+            <span className="count-of-fruits"> 0 </span>Bananas
+          </h1>
+          <div className="images-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+              alt="mango"
+              className="image-sizing"
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+              alt="banana"
+              className="image-sizing"
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FruitsCounter
